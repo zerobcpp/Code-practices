763ee7786880eec3ac4957fdd06ea9a0bd5c5508
SinceJuly <xyzzy021@gmail.com>
2023-04-07 03:23:26 -0400

    Time: 2010 ms (56.16%), Space: 104.9 MB (81.23%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,5 +1,5 @@
 class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+    def validPath1(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
         g = {i : [] for i in range(n)}
         for u, v in edges:
             g[u].append(v)
@@ -19,4 +19,31 @@ class Solution:
                     seen[neigh] = 1
         
         return False
+    
+    
+    def validPath(self, n, edges, source, destination) -> bool:
+        uf = UnionFind(n)
+        for a, b in edges:
+            uf.union(a, b)
+        print(uf.root, uf.rank)
+        return uf.find(source) == uf.find(destination)
                 
+        
+class UnionFind:
+    def __init__(self, n):
+        self.root = list(range(n))
+        self.rank = [1] * n
+    def find(self, x):
+        while self.root[x] != x:
+            self.root[x], x = self.root[self.root[x]], self.root[x]
+        return x
+    
+    def union(self, x, y):
+        root_x, root_y = self.find(x), self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                root_x, root_y = root_y, root_x
+            # Modify the root of the smaller group as the root of the
+            # larger group, also increment the size of the larger group.
+            self.rank[root_y] += self.rank[root_x]
+            self.root[root_x] = root_y
