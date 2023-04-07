b8bba5f9499c330edbeca7594203a3eee2b38863
SinceJuly <xyzzy021@gmail.com>
2023-04-07 03:33:04 -0400

    Time: 1992 ms (57.81%), Space: 105 MB (78.37%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -39,11 +39,11 @@ class UnionFind:
         return x
     
     def union(self, x, y):
-        root_x, root_y = self.find(x), self.find(y)
-        if root_x != root_y:
-            if self.rank[root_x] > self.rank[root_y]:
-                root_x, root_y = root_y, root_x
-            # Modify the root of the smaller group as the root of the
-            # larger group, also increment the size of the larger group.
-            self.rank[root_y] += self.rank[root_x]
-            self.root[root_x] = root_y
+        xr = self.find(x)
+        yr = self.find(y)
+        if (self.rank[xr] < self.rank[yr]): # Make yr parent of xr
+            self.root[xr] = self.root[yr]
+            self.rank[yr] += self.rank[xr]
+        else: # Make xr parent of yr
+            self.root[yr] = self.root[xr]
+            self.rank[xr] += self.rank[yr]
